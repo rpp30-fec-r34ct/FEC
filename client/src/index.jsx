6e27b0a4c725569e632bedabc207dcfa405865cb
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-06 12:09:19 -0700

    added getProductDetail function. The function accepts an id and sends a request to the server for the product information

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      productID: 18923
+      productID: 47421
     }
   }
 
   //use component did mount to trigger initial request for default product id data
   componentDidMount () {
+    this.getProductId(this.state.productID);
+  }
 
+  getProductDetails (id) {
+    axios.get(`http://localhost:3000/productDetail${id}`)
+    .then((data) => {
+      console.log('data received from server for product');
+    })
+    .catch ((err) => {
+      console.error ('error while getting product data from server');
+    })
   }
 
   render () {

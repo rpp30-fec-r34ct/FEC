26953e466166b61806d406f4a8858bf2073b0c4f
Laweeza <louieshing@gmail.com>
2021-09-06 16:41:18 -0700

    Create product cards component

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ProductCards from './ProductCards.jsx';
+import axios from 'axios';
 
 
 export default class Carousel extends React.Component {
@@ -8,6 +10,7 @@ export default class Carousel extends React.Component {
     return (
       <div className="carousel">
         <h2>Carousel</h2>
+        <ProductCards />
       </div>
     )
   }
---
+++ client/src/components/ProductCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class ProductCards extends React.Component {
+
+
+  render() {
+    return (
+      <div className="product-cards">
+        <h4>Product Cards</h4>
+      </div>
+    )
+  }
+}

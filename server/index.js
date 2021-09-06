b039ff91f1b2d5387d6cbbfcdc69784daa636727
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-06 10:58:59 -0700

    server serving index file in client/dist

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('client/dist'));
+
+
+app.listen(port, () => {
+  console.log(`Server listening http://localhost:${port}`);
+})
+

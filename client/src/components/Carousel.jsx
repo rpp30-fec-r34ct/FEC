8e9a44cde03ff3ef52b749611874dd2edaba8efe
Laweeza <louieshing@gmail.com>
2021-09-06 16:30:22 -0700

    Created Carousel componenet

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Carousel extends React.Component {
+
+
+  render() {
+    return (
+      <h2 className="carousel">Carousel</h2>
+    )
+  }
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Carousel from './components/Carousel.jsx';
 
 class App extends React.Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
       productID: 47421,
       productDetails: []
@@ -14,25 +15,25 @@ class App extends React.Component {
   }
 
   //use component did mount to trigger initial request for default product id data
-  componentDidMount () {
+  componentDidMount() {
     this.getProductDetails(this.state.productID);
   }
 
-  getProductDetails (id) {
+  getProductDetails(id) {
     axios.get(`http://localhost:3000/productDetail${id}`)
-    .then((data) => {
-      console.log('data received from server for product');
-      this.setState({
-        productID: data.data.id,
-        productDetails: data.data
+      .then((data) => {
+        console.log('data received from server for product');
+        this.setState({
+          productID: data.data.id,
+          productDetails: data.data
+        })
       })
-    })
-    .catch ((err) => {
-      console.error ('error while getting product data from server');
-    })
+      .catch((err) => {
+        console.error('error while getting product data from server');
+      })
   }
 
-  render () {
+  render() {
     return (
       <div>
         <h1>PROJECT ATLIER</h1>
@@ -40,9 +41,10 @@ class App extends React.Component {
         <div>{this.state.productDetails.name}</div>
         <h2>Product Description</h2>
         <div>{this.state.productDetails.description}</div>
+        <Carousel />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

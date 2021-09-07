ee58bc53318c04aacf1146546720c431d316a0ed
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-07 15:58:52 -0500

    Had to omit the ReactDOM mount for testing. This issue is being looked at in another PR. Added an export for App.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -45,4 +45,6 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+export default App;

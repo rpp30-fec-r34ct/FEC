afd2467ca23f20a5e6dfcd2c1e68396fc9511d97
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-07 17:15:58 -0500

    Added base structure to test with mock api calls. Built out original test to check if the api calls happen and render to the react page.

--- tests/components/index.test.js
+++ tests/components/index.test.js
@@ -2,16 +2,46 @@
  * @jest-environment jsdom
  */
 
-import React from 'react'
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
+import React from 'react';
+import 'regenerator-runtime/runtime';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node'
 
 // components
 import App from '../../client/src/index.jsx';
 
+// setup test data
+let testResponse = {
+  "id": 47421,
+  "campus": "hr-rpp",
+  "name": "Camo Onesie",
+  "slogan": "Blend in to your crowd",
+  "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+  "category": "Jackets",
+  "default_price": "140.00",
+  "created_at": "2021-08-26T20:30:48.129Z",
+  "updated_at": "2021-08-26T20:30:48.129Z",
+  "features": "[{…}, {…}]"
+}
+
+//setup worker
+const server = setupServer(
+  rest.get('/productDetail*', (req, res, ctx) => {
+    return res(ctx.json(testResponse))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
 describe('Index App', () => {
-  test('Should render the app title', () => {
-    render(<App/>);
-    expect(screen.getByText('PROJECT ATLIER')).toBeInTheDocument()
+  test('Should render the app title', async function() {
+    const app = render(<App/>);
+    expect(screen.getByText('PROJECT ATLIER')).toBeInTheDocument();
+    expect(await app.findByText(testResponse.name)).toBeInTheDocument();
+    expect(await app.findByText(testResponse.description)).toBeInTheDocument();
   });
 });

5a4ca27108d686dcb465e4d421bffde264faa648
Laweeza <louieshing@gmail.com>
2021-09-08 16:21:18 -0700

    Working on image display in carousel

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -17,6 +17,7 @@ export default function Carousel(props) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const length = products.length;
 
+
   // const getProducts = () =>
   //   fetch(`${fetchURL}/posts`)
   //     .then((res) => res.json())
@@ -25,38 +26,47 @@ export default function Carousel(props) {
   //   getProducts().then((data) => setData(data))
   // }, [])
 
-  const hideArrow = {
-    display: 'none'
-  }
 
 
   const nextCard = () => {
-    if (currentIndex >= 0 && currentIndex < products.length - 1) {
+    if (currentIndex >= 0 && currentIndex < (length - 1)) {
       setCurrentIndex(currentIndex => currentIndex + 1);
     }
   }
 
 
   const prevCard = () => {
-    if (currentIndex > 0 && currentIndex <= products.length - 1) {
+    if (currentIndex > 0 && currentIndex <= (length - 1)) {
       setCurrentIndex(currentIndex => currentIndex - 1);
     }
   }
 
 
   return (
-    <div className="carousel-container">
-      {currentIndex > 0 && <FaChevronLeft className="left-arrow" onClick={prevCard} />}
-      <div className="carousel-content" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
-        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
-        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
-        {/* {products?.map((item) =>
+    <div className="carousel-container" style={{
+      maxWidth: 300, marginLeft: 'auto', marginRight: 'auto', marginTop: 50
+    }} show={3}>
+      <div className="carousel-wrapper">
+        {currentIndex > 0 && <FaChevronLeft className="left-arrow" onClick={prevCard} />}
+        <div className="carousel-content-wrapper">
+          <div className="carousel-content" style={{ transform: `translateX(-${currentIndex * 80}%)` }}>
+
+            <img src="https://via.placeholder.com/50x100" alt="placeholder" />
+
+            <img src="https://via.placeholder.com/50x100" alt="placeholder" />
+
+            <img src="https://via.placeholder.com/50x100" alt="placeholder" />
+
+            <img src="https://via.placeholder.com/50x100" alt="placeholder" />
+
+            {/* {products?.map((item) =>
           <div key={item.id}>{item.name}</div>
-        )}
-      </div> */}
+          )}
+        </div> */}
+          </div>
+        </div>
+        {currentIndex < (length - 1) && <FaChevronRight className="right-arrow" onClick={nextCard} />}
       </div>
-      {currentIndex < (length - 1) && <FaChevronRight className="right-arrow" onClick={nextCard} />}
     </div>
   )
 

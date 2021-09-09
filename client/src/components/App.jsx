9f41e28df4712451641ec2c2356b757a80d4c1ea
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-08 22:31:43 -0500

    Refactored components to use React Hooks

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductDetailPage from './ProductDetailPage.jsx';
 
-class App extends React.Component {
-  constructor (props) {
-    super (props);
-  }
+const App = (props) => {
 
-  render () {
-    return (
-      <Router>
-        <h1>PROJECT ATLIER</h1>
-        <Switch>
+  return (
+    <Router>
+      <h1>PROJECT ATLIER</h1>
+      <Switch>
         <Route path="/product/:id" component={ProductDetailPage}/>
-        </Switch>
-      </Router>
-    )
-  }
+      </Switch>
+  </Router>
+  )
 }
 
 export default App;
--- client/src/components/ProductDetailPage.jsx
+++ client/src/components/ProductDetailPage.jsx
@@ -1,46 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ProductDetailPageComponent extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      productID: this.props.match.params.id,
-      productDetails: []
-    }
 
-    this.getProductDetails = this.getProductDetails.bind(this);
+const ProductDetailPageComponent = (props) => {
+  const [productDetails, setProductDetails] = useState({});
+
+  useEffect(() => {
+    getProductDetails();
+  },[])
+
+  let getProductDetails = () => {
+    axios.get(`http://localhost:3000/productDetail${props.match.params.id}`)
+    .then((data) => {
+      console.log('data received from server for product');
+      setProductDetails(data.data)
+    })
+    .catch ((err) => {
+      console.error ('error while getting product data from server');
+    })
   }
 
-    //use component did mount to trigger initial request for default product id data
-    componentDidMount () {
-      this.getProductDetails(this.state.productID);
-    }
-
-    getProductDetails (id) {
-      axios.get(`http://localhost:3000/productDetail${id}`)
-      .then((data) => {
-        console.log('data received from server for product');
-        this.setState({
-          productID: data.data.id,
-          productDetails: data.data
-        })
-      })
-      .catch ((err) => {
-        console.error ('error while getting product data from server');
-      })
-    }
-
-  render() {
-    return (
+  return (
       <div>
         <h2>Product Name</h2>
-        <div>{this.state.productDetails.name}</div>
+        <div>{productDetails.name}</div>
         <h2>Product Description</h2>
-        <div>{this.state.productDetails.description}</div>
+        <div>{productDetails.description}</div>
       </div>
     )
-  }
 }
 
 export default ProductDetailPageComponent;

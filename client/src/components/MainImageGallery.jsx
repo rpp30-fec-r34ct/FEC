391a119e8f0339698449a1feba0bf8d0aded2552
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-10 12:39:14 -0500

    Added initial component for Main Gallery with a place holder image

---
+++ client/src/components/MainImageGallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let MainImageGalleryComponent = (props) => {
+
+  return(
+    <div>
+      <h2>Image Gallery</h2>
+      <img src="https://via.placeholder.com/500"></img>
+    </div>
+  )
+}
+
+export default MainImageGalleryComponent;

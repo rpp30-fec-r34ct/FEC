40f6b9552953854a11b3038e8492bf178ab3a606
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-13 13:53:59 -0500

    Added the checkout containing component for product action buttons and drop downs.

---
+++ client/src/components/ProductCheckout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProductCheckoutComponent = (props) => {
+
+  return(
+    <div>
+      <select defaultValue="none">
+        <option value="none" disabled hidden>Select an Option</option>
+      </select>
+
+      <select>
+        <option value="1" >1</option>
+      </select>
+
+      <button>Add to bag    +</button>
+      <button>Favorite</button>
+
+    </div>
+  )
+}
+
+export default ProductCheckoutComponent;

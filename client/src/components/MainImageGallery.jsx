bbdbb5fa6985a1f0e437feb9405a10bbeb5aa92d
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-13 14:43:29 -0500

    Updated main gallery to load all images as thumbnails next to the main image

--- client/src/components/MainImageGallery.jsx
+++ client/src/components/MainImageGallery.jsx
@@ -1,14 +1,46 @@
 import React, { useEffect, useState } from "react";
 
 let MainImageGalleryComponent = (props) => {
-  const imgStyle = {
-    "hight": "500px",
-    "width": "500px"
+
+  const [selectedImage, setSelectedImage] = useState(props.selectedStyle.photos[0].url)
+
+  const containerStyle = {
+    "height": "500px",
+    "width": "500px",
+    "backgroundColor": "grey",
+    "display": "flex",
+    "justifyContent": "space-evenly"
   }
+
+  const imgStyle = {
+    "maxHeight": "100%",
+    "maxWidth": "100%"
+  }
+
+  const thumbnailStyle = {
+    "height": "75px",
+    "width": "75px",
+    "border": "solid, 2px, black",
+  }
+
+  const listStyles = {
+    "display": "flex",
+    "flexDirection": "column",
+    "alignItems": "center",
+    "justifyContent": "space-evenly"
+  }
+
   return(
     <div>
       <h2>Image Gallery</h2>
-      <img style={imgStyle} src={props.selectedStyle.photos[0].url}></img>
+      <div style={containerStyle}>
+        <div style={listStyles}>
+        {props.selectedStyle.photos.map((image, index) => {
+          return <img key={index} style={thumbnailStyle} src={image.thumbnail_url}></img>
+        })}
+        </div>
+        <img style={imgStyle} src={selectedImage}></img>
+      </div>
     </div>
   )
 }

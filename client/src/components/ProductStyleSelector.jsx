997251ef3b503e8ff5b0ca7d678c05366db9982c
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-13 12:54:49 -0500

    Completed basic skeleton for style selector images.

--- client/src/components/ProductStyleSelector.jsx
+++ client/src/components/ProductStyleSelector.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
+import StyleSelectorItem from './StyleSelectorItem.jsx';
 
 import axios from "axios";
 const ProductStyleComponent = (props) => {
   const [styles, setStyles] = useState([]);
-  const [selectedStyle, setSeleectedStyle] = useState({})
+  const [selectedStyle, setSelectedStyle] = useState({})
+  const [styleSelectors, setStyleSelectors] = useState([])
 
   const getStyles = () => {
     axios.get(`/api/products/${props.productId}/styles`)
       .then(res => {
         setStyles(res.data.results);
-        setSeleectedStyle(res.data.results[0]);
+        setSelectedStyle(res.data.results[0]);
       })
       .catch(err => {
         console.log(err);
@@ -19,9 +21,21 @@ const ProductStyleComponent = (props) => {
   useEffect(()=> {
     getStyles();
   }, [])
+
+  useEffect(()=> {
+    if (styles.length > 0) {
+      let newStyleSelectors = styles.map((style, index) => (
+        <StyleSelectorItem key={index} style={style}/>
+      ));
+      setStyleSelectors(newStyleSelectors);
+    }
+  }, [styles])
+
+
   return (
     <div>
       <h2> <b>style {'>'} </b>{selectedStyle.name}</h2>
+      {styleSelectors}
     </div>
   )
 }
---
+++ client/src/components/StyleSelectorItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StylesSelectorItemComponent = (props) => {
+  let styles = {
+    "height": "50px",
+    "width": "50px",
+    "borderRadius": "50%",
+    "border": "solid, 1px, black"
+  }
+  return(
+    <img style={styles} src={props.style.photos[0].thumbnail_url}></img>
+  )
+}
+
+export default StylesSelectorItemComponent;

283ae069ab2324c9c4a85f19b2c357b49ae51093
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-13 14:54:17 -0700

    added data to review tile component. Reviews can now display and conditionally render based on the response and recommended fields from the review Data. Minimal CSS has been added

--- client/src/components/ReviewTile.jsx
+++ client/src/components/ReviewTile.jsx
@@ -1,9 +1,74 @@
 import React, { useState } from 'react';
+import './ReviewSection.jsx'
+import '../cssFiles/reviewTile.css';
 
 const ReviewTile = (props) => {
-  return (
-    <h1>Hi from ReviewTile</h1>
-  );
+  if (props.reviewData.response === null) {
+    if (props.reviewData.recommend === false) {
+      return (
+        <div className="flex-container reviewTile">
+          <div className="tile_user_date_rating flex-container">
+          <span className="tile_rating">{props.reviewData.rating}</span>
+            <div className="tile_user_date">
+              <span className="tile_userName">{props.reviewData.reviewer_name}</span>
+              <span className="tile_date">{props.reviewData.date}</span>
+            </div>
+          </div>
+          <div className="tile_summary">{props.reviewData.summary}</div>
+          <div className="tile_body">{props.reviewData.body}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex-container reviewTile">
+          <div className="tile_user_date_rating flex-container">
+          <span className="tile_rating">{props.reviewData.rating}</span>
+            <div className="tile_user_date">
+              <span className="tile_userName">{props.reviewData.reviewer_name}</span>
+              <span className="tile_date">{props.reviewData.date}</span>
+            </div>
+          </div>
+          <div className="tile_summary">{props.reviewData.summary}</div>
+          <div className="tile_body">{props.reviewData.body}</div>
+          <div className="tile_recommend">I recommend this product</div>
+        </div>
+      );
+    }
+  } else {
+    if (props.reviewData.recommend === false) {
+      return (
+        <div className="flex-container reviewTile">
+          <div className="tile_user_date_rating flex-container">
+          <span className="tile_rating">{props.reviewData.rating}</span>
+            <div className="tile_user_date">
+              <span className="tile_userName">{props.reviewData.reviewer_name}</span>
+              <span className="tile_date">{props.reviewData.date}</span>
+            </div>
+          </div>
+          <div className="tile_summary">{props.reviewData.summary}</div>
+          <div className="tile_body">{props.reviewData.body}</div>
+          <div className="tile_response">Response: {props.reviewData.response}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex-container reviewTile">
+          <div className="tile_user_date_rating flex-container">
+          <span className="tile_rating">{props.reviewData.rating}</span>
+            <div className="tile_user_date">
+              <span className="tile_userName">{props.reviewData.reviewer_name}</span>
+              <span className="tile_date">{props.reviewData.date}</span>
+            </div>
+          </div>
+          <div className="tile_summary">{props.reviewData.summary}</div>
+          <div className="tile_body">{props.reviewData.body}</div>
+          <div className="tile_recommend">I recommend this product</div>
+          <div className="tile_response">Response: {props.reviewData.response}</div>
+        </div>
+      );
+    }
+  }
+
 };
 
 export default ReviewTile;

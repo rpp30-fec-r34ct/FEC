f80dd16ad85365f3568bb09cbe294416055c2eb2
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-13 12:26:02 -0700

    added route to retrieve reviews from API

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 const APIurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 const token = require('./config.js');
 
+// app.use('/product/:id', express.static('client/dist'));
 app.use(express.static('client/dist'));
-app.use('/product/:id', express.static('client/dist'));
 
 app.get('/productDetail*', (req, res) => {
   console.log('product details request received', req.url);
@@ -26,7 +26,26 @@ app.get('/productDetail*', (req, res) => {
   })
 });
 
+app.get('/reviews', (req, res) => {
+  console.log('got reviews request');
+  axios.get(APIurl + 'reviews', {
+    headers: {
+      'Authorization': token.API_KEY
+    },
+    params: {
+      sort: req.query.sort,
+      product_id: req.query.product_id
+    }
+  }).then ((data) => {
+    res.status(200).send(data.data);
+  })
+  .catch ((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  })
+});
+
 app.listen(port, () => {
   console.log(`Server listening http://localhost:${port}`);
-})
+});
 

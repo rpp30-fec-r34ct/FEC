365bf7ad67a0aa2d1ba070eda7e4920717d28416
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-13 12:26:43 -0700

    [MERGE CONFLICT LIKLEY] moved review section to the product details route because it's the only route I can get to work .... :)
    Will need to review this with the team

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductDetailPage from './ProductDetailPage.jsx';
+import ReviewSection from './ReviewSection.jsx';
 
 const App = (props) => {
 
@@ -11,8 +12,15 @@ const App = (props) => {
         <Route path="/product/:productId">
           <ProductDetailPage />
         </Route>
+        <Route path="/reviews">
+          <ReviewSection />
+        </Route>
+        <Route path="/">
+          <h1>Homepage</h1>
+          <ReviewSection product_id={47421}/>
+        </Route>
       </Switch>
-  </Router>
+     </Router>
   )
 }
 

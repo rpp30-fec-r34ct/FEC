8316f3587d05ef7d5eeaa11484f60b8dcbbcf632
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-13 13:54:41 -0500

    Updated main image gallery to load the first style image

--- client/src/components/MainImageGallery.jsx
+++ client/src/components/MainImageGallery.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 let MainImageGalleryComponent = (props) => {
-
+  const imgStyle = {
+    "hight": "500px",
+    "width": "500px"
+  }
   return(
     <div>
       <h2>Image Gallery</h2>
-      <img src="https://via.placeholder.com/500"></img>
+      <img style={imgStyle} src={props.selectedStyle.photos[0].url}></img>
     </div>
   )
 }

f75ab70480b19a9df41974350ea72a997f85dcb3
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-13 20:12:23 -0700

    added css file for the review section to help separate the review summary and review tiles

--- client/src/components/ReviewSection.jsx
+++ client/src/components/ReviewSection.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReviewList from './ReviewList.jsx';
+import ReviewMetaData from './ReviewMetaData.jsx';
+import '../cssFiles/reviewSection.css';
 
 const ReviewSection = (props) => {
 
@@ -9,7 +11,8 @@ const ReviewSection = (props) => {
 
   useEffect(() => {
       getReviews();
-  },[])
+      getReviewsMeta();
+  }, []);
 
 
   const getReviews = () => {
@@ -27,12 +30,27 @@ const ReviewSection = (props) => {
     });
   };
 
-
+  const getReviewsMeta = () => {
+    axios.get('http://localhost:3000/reviews/meta', {
+        params: {
+          product_id: props.product_id
+        }
+      })
+      .then ((data) => {
+        setReviewsMeta(data.data);
+      })
+      .catch ((error) => {
+      console.error(error);
+    });
+  };
 
   return (
     <div>
       <h3>Ratings and Reviews</h3>
-      <ReviewList reviews={reviews}/>
+      <div className="flex-container reviewSection">
+        <ReviewMetaData reviewsMeta={reviewsMeta}/>
+        <ReviewList reviews={reviews}/>
+      </div>
     </div>
   );
 };

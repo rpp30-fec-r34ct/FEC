5aa3136ac2bad2a82f27e247ed05766647158fdb
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-14 14:20:25 -0500

    Formatting fixes to align with standard js

--- server/index.js
+++ server/index.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const axios = require('axios');
-const APIurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
-const token = require('./config.js');
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios')
+const APIurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/'
+const token = require('./config.js')
 
-app.use(express.static('client/dist'));
-app.use('/product/:id', express.static('client/dist'));
+app.use(express.static('client/dist'))
+app.use('/product/:id', express.static('client/dist'))
 
 app.get('/api/*', async (req, res) => {
-  let path = req.url.split('/api/')[1]
+  const path = req.url.split('/api/')[1]
   try {
-    let response = await axios.get(APIurl + path,
+    const response = await axios.get(APIurl + path,
       {
         headers: {
-        'Authorization': token.API_KEY
+          Authorization: token.API_KEY
         }
       }
-    );
-    res.json(response.data);
-  } catch(err) {
-    res.status(500).send(err);
+    )
+    res.json(response.data)
+  } catch (err) {
+    res.status(500).send(err)
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server listening http://localhost:${port}`);
 })
 
+app.listen(port, () => {
+  console.log(`Server listening http://localhost:${port}`)
+})

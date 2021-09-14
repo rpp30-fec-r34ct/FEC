9aede7ade4cb7c00ccb74306b20ea4759c3f0366
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-13 21:57:44 -0500

    Fixed test bug where the component was having trouble processing the get request due to incorrect sample data and msw route name.

--- tests/components/ProductDetailPage.test.js
+++ tests/components/ProductDetailPage.test.js
@@ -17,16 +17,25 @@ import ProductDetailPage from '../../client/src/components/ProductDetailPage.jsx
 
 // setup test data
 let productResponse = {
-  "id": 47421,
-  "campus": "hr-rpp",
-  "name": "Camo Onesie",
-  "slogan": "Blend in to your crowd",
-  "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
-  "category": "Jackets",
-  "default_price": "140.00",
-  "created_at": "2021-08-26T20:30:48.129Z",
-  "updated_at": "2021-08-26T20:30:48.129Z",
-  "features": "[{…}, {…}]"
+    "id": 47421,
+    "campus": "hr-rpp",
+    "name": "Camo Onesie",
+    "slogan": "Blend in to your crowd",
+    "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+    "category": "Jackets",
+    "default_price": "140.00",
+    "created_at": "2021-08-26T20:30:48.129Z",
+    "updated_at": "2021-08-26T20:30:48.129Z",
+    "features": [
+        {
+            "feature": "Fabric",
+            "value": "Canvas"
+        },
+        {
+            "feature": "Buttons",
+            "value": "Brass"
+        }
+    ]
 }
 
 let productStyleResponse = {
@@ -411,7 +420,7 @@ describe('Product Detail Page', () => {
     const app = render(
       <Router history={history}>
         <Switch>
-          <Route path="/product/:id">
+          <Route path="/product/:productId">
             <ProductDetailPage />
           </Route>
         </Switch>

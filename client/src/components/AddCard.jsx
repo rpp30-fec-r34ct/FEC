206a9146f878220219f86224923b1f3ddbc3b768
Laweeza <louieshing@gmail.com>
2021-09-15 00:37:58 -0700

    resolved linting issue in addcard.jsx

--- client/src/components/AddCard.jsx
+++ client/src/components/AddCard.jsx
@@ -1,12 +1,12 @@
-/* eslint-disable */
 import React from 'react'
 import { AiOutlineCloseCircle, AiOutlinePlusCircle } from 'react-icons/ai'
 
-export default function AddCard(props) {
+export default function AddCard (props) {
   return (
     <div className='add-card'>
       <div className='add-details'>
         <div className='add-btn'><AiOutlinePlusCircle /></div>
+        <div className='delete-btn'><AiOutlineCloseCircle /></div>
         Add to Outfit
       </div>
     </div>

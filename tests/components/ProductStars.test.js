29b15c9a428d8047a6aedcf67b692e79512106ff
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-15 11:56:42 -0500

    Added new test to see if the correct inner fill percentage is being set

--- tests/components/ProductStars.test.js
+++ tests/components/ProductStars.test.js
@@ -12,6 +12,14 @@ describe('Product Stars Component', function () {
   test('Should render the stars', function () {
     const app = render(<ProductStars />)
     expect(app.container.querySelector('#starsOuter')).toBeInTheDocument()
-    expect(app.container.querySelectorAll('.fa-star').length).toBe(5)
+    expect(app.container.querySelector('#starsInner')).toBeInTheDocument()
+    expect(app.container.querySelectorAll('.fa-star').length).toBe(10)
+  })
+
+  test('Should render the correct inner color width based on the review', function () {
+    const app = render(<ProductStars rating={4.3} />)
+    const expectedRaitingRounded = Math.round((4.3 * 4) / 4)
+    const expectedRaitingPercentage = (expectedRaitingRounded / 5) * 100
+    expect(app.container.querySelector('#starsInner').style.width).toBe(`${expectedRaitingPercentage}%`)
   })
 })

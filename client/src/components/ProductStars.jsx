4fea8aca6d394e793153e6ff3cf270e0af35654b
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-15 11:33:45 -0500

    Refactored to dynamically fill stars based on the review to the nearest quarter. Still using a temp value. Need to refactor to take in a review count.

--- client/src/components/ProductStars.jsx
+++ client/src/components/ProductStars.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 
 const ProductStarsComponent = (props) => {
-  // const stars = <i class='fas fa-star' />
-
-  // const totalStars = 5
-  // const tempRating = 4.25
-  // const fillPercentage = (tempRating / totalStars) * 100
+  const totalStars = 5
+  const tempRating = 4.8
+  const roundedRating = Math.round((tempRating * 4) / 4).toFixed(2)
+  const fillPercentage = ((roundedRating / totalStars) * 100)
 
   const stylesOuter = {
     fontFamily: 'Font Awesome Free',
     fontWeight: 900,
-    color: 'black'
+    color: 'grey',
+    position: 'relative',
+    display: 'inline-block'
+  }
+
+  const stylesInner = {
+    fontFamily: 'Font Awesome Free',
+    fontWeight: 900,
+    color: 'orange',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: `${fillPercentage}%`,
+    overflow: 'hidden',
+    whiteSpace: 'nowrap'
   }
 
   return (
@@ -21,6 +34,13 @@ const ProductStarsComponent = (props) => {
         <i className='fas fa-star' />
         <i className='fas fa-star' />
         <i className='fas fa-star' />
+        <div id='starsInner' style={stylesInner}>
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+        </div>
       </div>
     </div>
   )

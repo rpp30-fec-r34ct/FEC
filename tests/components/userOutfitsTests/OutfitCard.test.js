44c9c961f9c263ed3b498937f74d521848ec22af
Laweeza <louieshing@gmail.com>
2021-09-15 22:12:16 -0700

    wrote smoke test for outfit card

--- tests/components/userOutfitsTests/OutfitCard.test.js
+++ tests/components/userOutfitsTests/OutfitCard.test.js
@@ -1,10 +1,20 @@
-// /**
-//  * @jest-environment jsdom
-//  */
+/**
+ * @jest-environment jsdom
+ */
 
-// import React from 'react'
-// import 'regenerator-runtime/runtime'
-// import { render } from '@testing-library/react'
-// import '@testing-library/jest-dom'
+import React from 'react'
+import 'regenerator-runtime/runtime'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-// import OutfitCard from '../../../client/src/components/userOutfits/OutfitCard.jsx'
+import OutfitCard from '../../../client/src/components/userOutfits/OutfitCard.jsx'
+
+describe('Outfit Card', () => {
+  test('renders category, name, and price in outfit card', () => {
+    render(<OutfitCard />)
+
+    expect(screen.getByText('CATEGORY'))
+    expect(screen.getByText('NAME'))
+    expect(screen.getByText('$PRICE'))
+  })
+})

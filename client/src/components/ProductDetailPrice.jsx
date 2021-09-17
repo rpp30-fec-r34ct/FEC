277c2cc65a273601c38e1b4fff26bf298c4402b0
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-17 14:51:21 -0500

    Added new component file for Product Price

---
+++ client/src/components/ProductDetailPrice.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductDetailPriceComponent = (props) => {
+  return (
+    <div>
+      {
+      props.sales_price
+        ? <p> </p>
+        : <p> </p>
+      }
+    </div>
+  )
+}
+
+export default ProductDetailPriceComponent

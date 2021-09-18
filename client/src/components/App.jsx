4f07be6ef20ea2137dbabe6c9685f8875bec874f
Oje Aisiku <oje.aisiku@gmail.com>
2021-09-18 12:07:22 -0400

    Added async test to QAList, fixed minor bug

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import ProductDetailPage from './ProductDetailPage.jsx'
-// import FourOhFour from './FourOhFour.jsx'
-// import Carousel from './Carousel.jsx'
-// import ReviewSection from './Review Widget/ReviewSection.jsx'
+import ProductDetailPage from './ProductDetailPage.jsx'
+import FourOhFour from './FourOhFour.jsx'
+import Carousel from './Carousel.jsx'
+import ReviewSection from './Review Widget/ReviewSection.jsx'
 import QAList from '../QAComponents/QAList.jsx'
 
 const App = (props) => {
@@ -12,12 +12,12 @@ const App = (props) => {
       <h1>PROJECT ATLIER</h1>
       <Switch>
         <Route path='/product/:productId' exact>
-          {/* <ProductDetailPage />
-          <Carousel /> */}
+          <ProductDetailPage />
+          <Carousel />
           <QAList />
-          {/* <ReviewSection /> */}
+          <ReviewSection />
         </Route>
-        {/* <Route path='/product/:productId/carousel/'>
+        <Route path='/product/:productId/carousel/'>
           <Carousel />
         </Route>
         <Route path='/404'>
@@ -28,7 +28,7 @@ const App = (props) => {
         </Route>
         <Route path='/product/:productId/questions'>
           <QAList />
-        </Route> */} */}
+        </Route> */}
       </Switch>
     </Router>
   )

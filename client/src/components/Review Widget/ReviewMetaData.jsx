9811297957d672d3d66faa3adbb8ca07376ccacd
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-21 01:24:01 -0700

    added default data back in that allows the bar chart full bar to render

--- client/src/components/Review Widget/ReviewMetaData.jsx
+++ client/src/components/Review Widget/ReviewMetaData.jsx
@@ -1,91 +1,3 @@
-// import React, { useState, useEffect } from 'react'
-// import axios from 'axios'
-// import './cssFiles/reviewSection.css'
-// import ReviewStars from './ReviewStars.jsx'
-// import BarChart from './BarChart.jsx';
-// import ProductBreakdown from './productBreakdown.jsx';
-
-// const ReviewMetaData = (props) => {
-//   const [reviewsMeta, setReviewsMeta] = useState({
-//     average: 0,
-//     percentRecommend: 0,
-//     totalReviews: 0
-//   })
-
-//   useEffect(() => {
-//     getReviewsMeta()
-//   }, [])
-
-//   const getReviewsMeta = () => {
-//     axios.get('/reviews/meta', {
-//       params: {
-//         product_id: props.product_id
-//       }
-//     })
-//     .then((data) => {
-//       let [average, reviewCount] = calculateAverage(data.data)
-//       let percent = calculatePercentRecommend(data.data.recommended.false, data.data.recommended.true)
-
-//       setAllData(data, average, percent, reviewCount);
-//     })
-//     .catch((error) => {
-//       console.error(error)
-//     })
-//   }
-
-//   const calculateAverage = (data) => {
-//     let currentAverage = 0
-//     let reviewCount = 0
-
-//     for (const key in data.ratings) {
-//       currentAverage = currentAverage + parseInt(key) * parseInt(data.ratings[key])
-//       reviewCount = reviewCount + parseInt(data.ratings[key])
-//     }
-//     return ([Math.round((currentAverage / reviewCount) * 10) / 10, reviewCount])
-//   }
-
-//   const calculatePercentRecommend = (falseCount, trueCount) => {
-//     const noCount = parseInt(falseCount)
-//     const yesCount = parseInt(trueCount)
-
-//     return Math.round((yesCount / (noCount + yesCount === 0 ? 1 : (noCount + yesCount))) * 100)
-//   }
-
-//   const setAllData = (data, average, percent, reviewCount) => {
-//     setReviewsMeta({
-//       reviewsMeta: data,
-//       average: average,
-//       percentRecommend: percent,
-//       totalReviews: reviewCount
-//     })
-//   }
-
-//   if (reviewsMeta.reviewsMeta === undefined) {
-//     return (null);
-//   } else {
-//     return (
-//       <>
-//         <div className='reviewSummary'>
-//           <div data-testid='testAverage' className='ratingItem'>{reviewsMeta.average}</div>
-//           <ReviewStars data-testid='testAverageStars' starRating={reviewsMeta.average} review_id={reviewsMeta.reviewsMeta.product_id} />
-//         </div>
-//         <div>
-//           <span data-testid='testPercent' className='percentRecommend'>{reviewsMeta.percentRecommend + '% of reviews recommend this product'}</span><br></br>
-//         </div>
-//         <div className="ratingBreakDown">
-//           <span className="ratingBreakdownTitle" >Rating Breakdown</span>
-//           <BarChart breakDownRatings={reviewsMeta.reviewsMeta.ratings} totalReviews={reviewsMeta.totalReviews}/>
-//         </div>
-//         <ProductBreakdown characteristicsData={reviewsMeta.reviewsMeta.characteristics} />
-//       </>
-
-//     )
-//   }
-
-// }
-
-// export default ReviewMetaData
-
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './cssFiles/reviewSection.css'
@@ -107,7 +19,8 @@ const ReviewMetaData = (props) => {
       }
     },
     average: 0,
-    percentRecommend: 0
+    percentRecommend: 0,
+    totalReviews: 0
   })
 
   useEffect(() => {
@@ -128,14 +41,15 @@ const ReviewMetaData = (props) => {
       })
   }
 
-  const calculatePercentRecommend = (currentAverage, data) => {
+  const calculatePercentRecommend = (currentAverage, data, reviewCount) => {
     const noCount = parseInt(data.recommended.false)
     const yesCount = parseInt(data.recommended.true)
 
     setReviewsMeta({
       reviewsMeta: data,
       average: currentAverage,
-      percentRecommend: Math.round((yesCount / (noCount + yesCount === 0 ? 1 : (noCount + yesCount))) * 100)
+      percentRecommend: Math.round((yesCount / (noCount + yesCount === 0 ? 1 : (noCount + yesCount))) * 100),
+      totalReviews: reviewCount
     })
   }
 
@@ -149,7 +63,7 @@ const ReviewMetaData = (props) => {
     }
 
     currentAverage = Math.round((currentAverage / reviewCount) * 10) / 10
-    calculatePercentRecommend(currentAverage, data)
+    calculatePercentRecommend(currentAverage, data, reviewCount)
   }
 
   return (
@@ -162,7 +76,7 @@ const ReviewMetaData = (props) => {
         <span data-testid='testPercent' className='percentRecommend'>{reviewsMeta.percentRecommend + '% of reviews recommend this product'}</span>
       </div>
       <div className="ratingBreakDown">
-        <span className="ratingBreakdownTitle" >Rating Breakdown</span>
+        <span className="ratingBreakdownTitle">Rating Breakdown</span>
         <BarChart breakDownRatings={reviewsMeta.reviewsMeta.ratings} totalReviews={reviewsMeta.totalReviews}/>
       </div>
         <ProductBreakdown characteristicsData={reviewsMeta.reviewsMeta.characteristics} />

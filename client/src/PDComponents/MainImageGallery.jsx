13b7dd17e536b388df45163e9bae6619849a8eee
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-22 13:40:56 -0500

    Refactored to include new Carousel sub component
    Also added a default render until data is received in parent component

--- client/src/PDComponents/MainImageGallery.jsx
+++ client/src/PDComponents/MainImageGallery.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { FaChevronUp, FaChevronDown } from 'react-icons/fa'
+// import { FaChevronUp, FaChevronDown } from 'react-icons/fa'
+import ThumbnailCarousel from './ThumbnailCarousel.jsx'
 
 const MainImageGalleryComponent = (props) => {
   const [selectedStyle, setSelectedStyle] = useState(false)
-  const [selectedImage, setSelectedImage] = useState()
-  const [thumbnails, setThumbnails] = useState([])
-  const [topIndex, setTopIndex] = useState(0)
+  const [selectedImage, setSelectedImage] = useState(null)
+  // const [topIndex, setTopIndex] = useState(0)
 
   useEffect(() => {
     if (props.selectedStyle !== false) {
@@ -14,30 +14,22 @@ const MainImageGalleryComponent = (props) => {
     }
   }, [props])
 
-  useEffect(() => {
-    if (selectedStyle !== false) {
-      setThumbnails(selectedStyle.photos.map((image, index) => {
-        return <img key={index} data-index={index} style={thumbnailStyle} src={image.thumbnail_url} onClick={imageClickHandler} />
-      }))
-    }
-  }, [selectedStyle])
-
   const imageClickHandler = (e) => {
     e.preventDefault()
     setSelectedImage(selectedStyle.photos[e.target.getAttribute('data-index')].url)
   }
 
-  const handleCarouselUpClick = () => {
-    setTopIndex(prevIndex => {
-      return prevIndex - 1
-    })
-  }
+  // const handleCarouselUpClick = () => {
+  //   setTopIndex(prevIndex => {
+  //     return prevIndex - 1
+  //   })
+  // }
 
-  const handleCarouselDownClick = () => {
-    setTopIndex(prevIndex => {
-      return prevIndex + 1
-    })
-  }
+  // const handleCarouselDownClick = () => {
+  //   setTopIndex(prevIndex => {
+  //     return prevIndex + 1
+  //   })
+  // }
 
   const containerStyle = {
     height: '500px',
@@ -53,43 +45,34 @@ const MainImageGalleryComponent = (props) => {
     objectFit: 'cover'
   }
 
-  const thumbnailStyle = {
-    height: '75px',
-    width: '75px',
-    marginBottom: '4px',
-    border: 'solid, 2px, black',
-    objectFit: 'cover'
+  const skeletonImg = {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+    backgroundColor: 'Gainsboro'
   }
 
-  const listStyles = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    overflow: 'hidden'
-  }
+  // const listStyles = {
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   alignItems: 'center',
+  //   overflow: 'hidden'
+  // }
 
-  const carouselListStyles = {
-    transform: `translateY(-${topIndex * 77}px)`,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly'
-  }
+  // const carouselListStyles = {
+  //   transform: `translateY(-${topIndex * 77}px)`,
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   alignItems: 'center',
+  //   justifyContent: 'space-evenly'
+  // }
 
   return (
     <div>
       <div style={containerStyle}>
-        <div style={listStyles}>
-          {(topIndex > 0) ? <FaChevronUp className='up-arrow' onClick={handleCarouselUpClick} /> : <FaChevronUp className='hidden' />}
-          <div style={{ maxHeight: '395px', overflow: 'hidden' }}>
-            <div style={carouselListStyles}>
-              {thumbnails}
-            </div>
-          </div>
-          {(topIndex < thumbnails.length - 5) ? <FaChevronDown className='down-arrow' onClick={handleCarouselDownClick} /> : <FaChevronDown className='hidden' />}
-        </div>
+        <ThumbnailCarousel selectedStyle={selectedStyle} imageClickHandler={imageClickHandler} />
         <div style={{ width: '400px', height: '500px', overflow: 'hidden' }}>
-          <img style={imgStyle} src={selectedImage} />
+          {selectedImage ? <img style={imgStyle} src={selectedImage} /> : <img style={skeletonImg} />}
         </div>
       </div>
     </div>

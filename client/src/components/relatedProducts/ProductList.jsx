ea194aa683916614d7098f05f1d5f3642770c6d5
Laweeza <louieshing@gmail.com>
2021-09-22 18:15:07 -0700

    fixing translate

--- client/src/components/relatedProducts/ProductList.jsx
+++ client/src/components/relatedProducts/ProductList.jsx
@@ -5,7 +5,7 @@ export default function ProductList(props) {
   return (
     <div className='product-list'>
       <div className='carousel-content-wrapper'>
-        <div className='carousel-content' style={{ transform: `translateX(${props.currentIndex * 2}%)` }}>
+        <div className='carousel-content' style={{ transform: `translateX(${props.currentIndex * 15}%)` }}>
           {
             props.relatedProducts.map((product, index) => {
               return <ProductCards

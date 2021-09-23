948c4b01fd9675b25ce15f34d8f7f3b219161942
Laweeza <louieshing@gmail.com>
2021-09-22 21:53:52 -0700

    working on mousetracker

--- client/src/components/MouseTracker.jsx
+++ client/src/components/MouseTracker.jsx
@@ -2,16 +2,27 @@ import { useState, useEffect } from 'react';
 
 const useMouse = () => {
   const [mouseLocation, setMouseLocation] = useState({ x: 0, y: 0 });
+  const [element, setElement] = useState(null);
 
   useEffect(() => {
-    const listener =
-      window.addEventListener('mousemove',
-        e => setMouseLocation({ x: e.clientX, y: e.clientY }))
+    const moveListener =
+      window.addEventListener('mousemove', (e) =>
+        setMouseLocation({ x: e.clientX, y: e.clientY })
+      );
 
-    return () => window.removeEventListener('mousemove', listener);
-  }, [])
+    const clickListener =
+      window.addEventListener('click', (e) =>
+        setElement(e.target.toString())
+      );
 
-  return mouseLocation;
-}
+
+    return () => {
+      window.removeEventListener('mousemove', listener);
+      window.removeEventListener('click', clickListener);
+    };
+  }, []);
+
+  return { ...mouseLocation, element };
+};
 
 export default useMouse;

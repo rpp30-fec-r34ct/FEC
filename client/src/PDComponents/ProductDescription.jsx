a47a34d78500bb376c2bb9a20348bf3a8fd71e22
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-24 11:59:31 -0500

    Added selectedStyle to checkout props

--- client/src/PDComponents/ProductDescription.jsx
+++ client/src/PDComponents/ProductDescription.jsx
@@ -3,20 +3,20 @@ import ProductCheckout from './ProductCheckout.jsx'
 import ProductStars from './ProductStars.jsx'
 import ProductStyleSelector from './ProductStyleSelector.jsx'
 
-const ProductDescriptionComponent = (props) => {
-  const styles = {
+const ProductDescriptionComponent = ({ productDetails, selectedStyle, styles, updateSelectedStyle }) => {
+  const descriptionStyles = {
     display: 'flex',
     flexDirection: 'column',
     marginLeft: '20px'
   }
   return (
-    <div style={styles}>
+    <div style={descriptionStyles}>
       <ProductStars />
-      <h4>{props.productDetails.category}</h4>
-      <h2>{props.productDetails.name}</h2>
-      <p>${props.productDetails.default_price}</p>
-      <ProductStyleSelector styles={props.styles} selectedStyle={props.selectedStyle} updateSelectedStyle={props.updateSelectedStyle} />
-      <ProductCheckout />
+      <h4>{productDetails.category}</h4>
+      <h2>{productDetails.name}</h2>
+      <p>${productDetails.default_price}</p>
+      <ProductStyleSelector styles={styles} selectedStyle={selectedStyle} updateSelectedStyle={updateSelectedStyle} />
+      <ProductCheckout selectedStyle={selectedStyle} />
     </div>
   )
 }

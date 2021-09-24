c9c33017d021a187afe1a69a5b069f31326dbb88
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-24 13:41:48 -0500

    Refactoed SizeSelector to render the sizes dynamically
    If selectedStyle is null, this will render an out of stock disabled selector. else it will render all skus and disable those that are not in stock.

--- client/src/PDComponents/SizeSelector.jsx
+++ client/src/PDComponents/SizeSelector.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import SizeSelectorItemComponent from './SizeSelectorItem.jsx'
 
-const SizeSelectorComponent = ({ selectedStyle }) => {
+const SizeSelectorComponent = ({ selectedStyle, updateSelectedSize }) => {
+  const skus = []
+  if (selectedStyle) {
+    for (const size in selectedStyle.skus) {
+      skus.push(<SizeSelectorItemComponent item={selectedStyle.skus[size]} name={size} />)
+    }
+  }
+
   return (
     <>
       {selectedStyle
         ? (
-          <select disabled>
-            <option value='OUT OF STOCK' disabled='disabled' default='true'>OUT OF STOCK</option>
+          <select onChange={updateSelectedSize} defaultValue='0'>
+            <option value='0' disabled>Select Size</option>
+            {skus}
           </select>
           )
+
         : (
           <select disabled defaultValue='OUT OF STOCK'>
-            <option value='OUT OF STOCK' disabled='disabled' default='true'>OUT OF STOCK</option>
+            <option value='OUT OF STOCK' disabled='disabled'>OUT OF STOCK</option>
           </select>
           )}
     </>

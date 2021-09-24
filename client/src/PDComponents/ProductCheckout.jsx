aa66e95c392a2a25963368040ef55164d244ca93
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-24 13:38:39 -0500

    WIP refactored to handle selector functionality
    Added wrapper divs for styling. Set a new state for the selected size. Added the new QuanitySelector component and passed it the currently selected size

--- client/src/PDComponents/ProductCheckout.jsx
+++ client/src/PDComponents/ProductCheckout.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SizeSelector from './SizeSelector.jsx'
+import QuanitiySelector from './QuanitySelector.jsx'
 
 const ProductCheckoutComponent = ({ selectedStyle }) => {
+  const [selectedSize, setSelectedSize] = useState(null)
+
+  const handleSizeChange = (e) => {
+    e.preventDefault()
+    const sizeId = e.target.value
+    updateSelectedSize(selectedStyle.skus[sizeId])
+  }
+
+  const updateSelectedSize = (newSelection) => {
+    setSelectedSize(newSelection)
+  }
+
+  const checkoutStyles = {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+
+  const containerStyles = { display: 'flex', justifyContent: 'space-evenly' }
+
   return (
-    <div>
-      <SizeSelector selectedStyle={selectedStyle} />
+    <div style={checkoutStyles}>
+      <div style={containerStyles}>
+        <SizeSelector selectedStyle={selectedStyle} updateSelectedSize={handleSizeChange} />
 
-      <select>
-        <option value='1'>1</option>
-      </select>
+        <QuanitiySelector selectedSize={selectedSize} />
+      </div>
 
-      <button>Add to bag    +</button>
-      <button>Favorite</button>
+      <div style={containerStyles}>
+        <button>Add to bag    +</button>
+        <button>Favorite</button>
+      </div>
 
     </div>
   )

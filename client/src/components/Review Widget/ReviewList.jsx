59e69809b3a599e7518247a6538762da1cbd6ac0
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-25 13:50:36 -0700

    refactored how the client requests and stores reviews so that the client can do less work. majority of the work happens on the server side now.
    also - there's something strange going on with the API where there is a mismatch between reviews and ratings. That's not supposed to be possible per the spec, and it's messing up the conditional rendering to tell if 'more reviews' button should be available or not. I have a rather poor work around in place right now so the more reviews will disappear if the server doesn't send any review back. i'm reaching out to Alex for clarification on whether we need to accommodate having a mismatch between reviews/ratings (deviation from spec) or not
    
    will come back and fix the workaround based on Alex's answer

--- client/src/components/Review Widget/ReviewList.jsx
+++ client/src/components/Review Widget/ReviewList.jsx
@@ -6,25 +6,41 @@ import './cssFiles/reviewSection.css'
 const ReviewList = (props) => {
   const [reviews, setReviews] = useState([])
   const [reviewDisplayCount, setDisplayCount] = useState(2)
+  const [sortType, setSortType] = useState('relevance')
+  const [pullMoreReviews, setPullMoreReviews] = useState(1)
 
   useEffect(() => {
     getReviews(reviewDisplayCount, props.product_id)
   }, [])
 
+
+
   const getReviews = (count, productId) => {
     axios.get('/reviews', {
       params: {
         count: count,
+        sort: sortType,
         product_id: productId
       }
     })
       .then((data) => {
-        // setReviews(data.data.results)
-        let reviews = data.data.results;
+        //this is protection for something i do not understand about the API yet. the API right now tells me there are 21 ratings for product 47421
+        //and according to my understanding, you cannot have a rating without a review, so i should also be able to pull down 21 reviews for display
+        //hOWEVER - the API will only give me 8 reviews. If i ask it for more than 8 on the same page it just returns 8. If i ask it for 8 on page 2, it for some reason skips to page 8 and returns no revies
+        //that leads me to believe there is not a rating to review ratio of 1:1 even though there should be from what the spec says.
+        //so - > here I am overwriting the maximum # of reviews so the uI knows to stop offering the 'more reviews button' if the client gets
+        //no reviews after requesting them. i'll take this out once i understand why the rating count given by the  API doesn't match the review count
+
+        if (data.data.length < 2) {
+          setPullMoreReviews(0);
+        }
+
+        let newReviews = data.data;
         const reviewListTiles = []
-        reviews.map((reviewData) => { return (reviewListTiles.push(<ReviewTile key={reviewData.review_id} reviewData={reviewData} />)) })
+        newReviews.map((reviewData) => { return (reviewListTiles.push(<ReviewTile key={reviewData.review_id} reviewData={reviewData} />)) })
         // return reviewListTiles;
-        setReviews(reviewListTiles)
+        setReviews(reviews.concat(reviewListTiles))
+
       })
       .catch((error) => {
         console.error(error)
@@ -32,7 +48,7 @@ const ReviewList = (props) => {
   }
 
   const moreReviews = () => {
-    if (reviewDisplayCount < props.totalReviews) {
+    if (reviewDisplayCount < props.totalReviews && pullMoreReviews) {
       setDisplayCount(reviewDisplayCount + 2)
       getReviews(reviewDisplayCount + 2, props.product_id);
     } else {

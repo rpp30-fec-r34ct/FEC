9ae9163a733684c5cd6510922906edc8d1303df8
Laweeza <louieshing@gmail.com>
2021-09-25 09:04:30 -0700

    trying to imeplement mousetracker

--- client/src/components/ClickTracker.jsx
+++ client/src/components/ClickTracker.jsx
@@ -1,13 +1,14 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useRef } from 'react'
+
 
 const ClickTrackerComponent = (props) => {
   const submitTrackedInfo = (e, widget) => {
-    console.log(e.target)
+    console.log('?', e.target)
     axios.post('/api/interactions', {
       element: `${e.target.outerHTML}`,
       time: `${Date.now()}`,
-      widget: widget
+      widget: 'widget'
     })
       .then(response => console.log(response.data))
       .catch(err => console.log(err))

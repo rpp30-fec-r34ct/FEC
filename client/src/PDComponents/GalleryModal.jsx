371d640f052a1884972804c49650cbaada13dac5
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-27 18:10:18 -0500

    Added innerzoomimage component for testing

--- client/src/PDComponents/GalleryModal.jsx
+++ client/src/PDComponents/GalleryModal.jsx
@@ -1,27 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css'
 import './styles/GalleryModal.css'
 import { FaChevronLeft, FaChevronRight, FaCompress } from 'react-icons/fa'
+import InnerImageZoom from 'react-inner-image-zoom'
 
 const GalleryModal = ({ selectedStyle, currentIndex, handleCollapseImageClick, handleClickOffModal }) => {
   const [imgIndex, setImgIndex] = useState(currentIndex)
-  const [isZoomed, setIsZoomed] = useState(false)
-  const [mousePosition, setMousePosition] = useState({ x: null, y: null })
-
-  const updateMousePosition = e => {
-    const img = document.getElementById('modal-main-image')
-    const imgX = img.scrollWidth
-    const imgY = img.scrollHeight
-
-    setMousePosition({ x: imgX - e.clientX, y: imgY - e.clientY })
-  }
-
-  useEffect(() => {
-    if (isZoomed) {
-      window.addEventListener('mousemove', updateMousePosition)
-
-      return () => window.removeEventListener('mousemove', updateMousePosition)
-    }
-  }, [isZoomed])
 
   const handleCarouselLeftClick = () => {
     setImgIndex(prevIndex => {
@@ -35,27 +19,12 @@ const GalleryModal = ({ selectedStyle, currentIndex, handleCollapseImageClick, h
     })
   }
 
-  const handleImageZoomClick = (e) => {
-    e.target.classList.toggle('zoom-in-img')
-    setIsZoomed(prevState => !prevState)
-  }
-
-  const zoomStyles = {
-    transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`
-  }
-
   return (
     <div id='gallery-modal' onClick={handleClickOffModal}>
       <div id='gallery-modal-content'>
         <FaCompress id='gallery-modal-close' onClick={handleCollapseImageClick} />
         {(imgIndex > 0) ? <FaChevronLeft className='gallery-modal-arrow' onClick={handleCarouselLeftClick} /> : <FaChevronLeft className='hidden gallery-modal-arrow' />}
-        <div id='modal-main-img'>
-          { isZoomed
-            ? <img style={zoomStyles} className='gallery-modal-image' id='modal-main-image' src={selectedStyle.photos[imgIndex].url} onClick={handleImageZoomClick} />
-            : <img className='gallery-modal-image' id='modal-main-image' src={selectedStyle.photos[imgIndex].url} onClick={handleImageZoomClick} />
-          }
-
-        </div>
+        <InnerImageZoom src={selectedStyle.photos[imgIndex].url} zoomScale={2} />
         {(imgIndex < selectedStyle.photos.length - 1) ? <FaChevronRight className='gallery-modal-arrow' onClick={handleCarouselRightClick} /> : <FaChevronRight className='hidden gallery-modal-arrow' />}
       </div>
     </div>

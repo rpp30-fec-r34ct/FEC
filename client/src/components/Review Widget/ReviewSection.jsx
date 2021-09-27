3219c5c1b8b9f2de606b077b140b29652c71e681
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-26 23:19:36 -0700

    added state to host the active star rating filters that the user can select in the Barchart component. Added click handler to update the star rating filter state when new filters are selected Added props to the ReviewList render so the activeFilters are shared with the reviewList component when they are updated

--- client/src/components/Review Widget/ReviewSection.jsx
+++ client/src/components/Review Widget/ReviewSection.jsx
@@ -8,7 +8,6 @@ import axios from 'axios';
 const ReviewSection = () => {
   const { productId } = useParams()
 
-
   const [reviewsMeta, setReviewsMeta] = useState({
     ratings: null,
     recommended: {
@@ -16,6 +15,14 @@ const ReviewSection = () => {
       true: 0
     },
     product_id: productId
+  })
+
+  const [activeFilters, setActiveFilters] = useState({
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+    5: true
   })
 
 
@@ -41,12 +48,65 @@ const ReviewSection = () => {
     let reviewCount = 0;
 
     if (reviewsMeta.ratings !== null) {
-      for (const key in reviewsMeta.ratings) {
+      for (let key in reviewsMeta.ratings) {
         reviewCount = reviewCount + parseInt(reviewsMeta.ratings[key])
       }
     }
-
     return reviewCount;
+  }
+
+  const handleFilterToggle = (event) => {
+    let ratingSelected = event.target.innerText[0]
+    let newFilters = {};
+    let trueCount = 0;
+    Object.assign(newFilters, activeFilters)
+    let allActive = true;
+
+    for (var key in newFilters) {
+      if (newFilters[key] === false) {
+        allActive = false;
+      }
+    }
+    if (allActive) {
+      for (var key in newFilters) {
+        if (key !== ratingSelected) {
+          newFilters[key] = false;
+        }
+      }
+    } else {
+      for (var key in newFilters) {
+        if (key === ratingSelected) {
+          newFilters[key] ? newFilters[key] = false : newFilters[key] = true
+        }
+        if (newFilters[key]) {
+          trueCount = trueCount + 1;
+        }
+      }
+
+      //once we've toggled the filters as requested, if we are back to having all filters set to false, then we are in a default state. Turn all filters to true
+      if (trueCount === 0) {
+        newFilters = {
+          1: true,
+          2: true,
+          3: true,
+          4: true,
+          5: true
+        }
+      }
+    }
+
+    setActiveFilters(newFilters);
+  }
+
+  const clearAllFilters = () => {
+    let newFilters = {
+      1: true,
+      2: true,
+      3: true,
+      4: true,
+      5: true
+    }
+    setActiveFilters(newFilters);
   }
 
 
@@ -54,8 +114,8 @@ const ReviewSection = () => {
     <div>
       <h3>Ratings and Reviews</h3>
       <div className='reviewSection'>
-        <ReviewMetaData metaData={reviewsMeta} totalReviews={getTotalReviews()}/>
-        <ReviewList product_id={productId} totalReviews={getTotalReviews()} />
+        <ReviewMetaData metaData={reviewsMeta} totalReviews={getTotalReviews()} handleFilterToggle={handleFilterToggle}/>
+        <ReviewList product_id={productId} totalReviews={getTotalReviews()} activeFilters={activeFilters} clearAllFilters={clearAllFilters}/>
       </div>
     </div>
   )

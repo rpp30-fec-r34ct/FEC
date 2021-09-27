fe4d862096149764a95a21c75e017e35c13fda69
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-27 17:19:32 -0500

    Adjusted styles for gallery modal.

--- client/src/PDComponents/GalleryModal.jsx
+++ client/src/PDComponents/GalleryModal.jsx
@@ -17,12 +17,18 @@ const GalleryModal = ({ selectedStyle, currentIndex, handleCollapseImageClick, h
     })
   }
 
+  const handleImageZoomClick = (e) => {
+    e.target.classList.toggle('zoom-in-img')
+  }
+
   return (
     <div id='gallery-modal' onClick={handleClickOffModal}>
       <div id='gallery-modal-content'>
         <FaCompress id='gallery-modal-close' onClick={handleCollapseImageClick} />
         {(imgIndex > 0) ? <FaChevronLeft className='gallery-modal-arrow' onClick={handleCarouselLeftClick} /> : <FaChevronLeft className='hidden gallery-modal-arrow' />}
-        <img id='gallery-modal-image' src={selectedStyle.photos[imgIndex].url} />
+        <div id='modal-main-img'>
+          <img className='gallery-modal-image' src={selectedStyle.photos[imgIndex].url} onClick={handleImageZoomClick} />
+        </div>
         {(imgIndex < selectedStyle.photos.length - 1) ? <FaChevronRight className='gallery-modal-arrow' onClick={handleCarouselRightClick} /> : <FaChevronRight className='hidden gallery-modal-arrow' />}
       </div>
     </div>

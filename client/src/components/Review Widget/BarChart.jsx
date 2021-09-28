415fa7e8952d9eb90cf083a8af2e12b07f38b797
Ashley Reischman <ashleyreischman648@gmail.com>
2021-09-28 01:42:34 -0700

    npx fix

--- client/src/components/Review Widget/BarChart.jsx
+++ client/src/components/Review Widget/BarChart.jsx
@@ -5,13 +5,13 @@ const BarChart = (props) => {
   const bars = []
 
   if (props.breakDownRatings === null) {
-    for (var i = 5; i > 0; i--) {
+    for (let i = 5; i > 0; i--) {
       bars.push(<BarLine key={i} star={i.toString()} numberOfReviews={0} />)
     }
   } else {
     const givenStars = Object.keys(props.breakDownRatings)
 
-    for (var i = 5; i > 0; i--) {
+    for (let i = 5; i > 0; i--) {
       if (givenStars.indexOf(i.toString()) === -1) {
         bars.push(<BarLine key={i} star={i.toString()} numberOfReviews={0} handleFilterToggle={props.handleFilterToggle} />)
       } else {

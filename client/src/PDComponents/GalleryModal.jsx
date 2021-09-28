be8e0489344b8ee7a6c66f6c2d71e8e721eee969
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-28 00:10:51 -0500

    WIP added the sub carousel to the modal. Need to restyle

--- client/src/PDComponents/GalleryModal.jsx
+++ client/src/PDComponents/GalleryModal.jsx
@@ -3,6 +3,7 @@ import './styles/ImageZoomStyles.css'
 import './styles/GalleryModal.css'
 import { FaChevronLeft, FaChevronRight, FaCompress } from 'react-icons/fa'
 import InnerImageZoom from 'react-inner-image-zoom'
+import SubImageCarousel from './SubImageCarousel.jsx'
 
 const GalleryModal = ({ selectedStyle, currentIndex, handleCollapseImageClick, handleClickOffModal }) => {
   const [imgIndex, setImgIndex] = useState(currentIndex)
@@ -19,15 +20,23 @@ const GalleryModal = ({ selectedStyle, currentIndex, handleCollapseImageClick, h
     })
   }
 
+  const imageClickHandler = (e) => {
+    e.preventDefault()
+    setImgIndex(parseInt(e.target.getAttribute('data-index')))
+  }
+
   return (
     <div id='gallery-modal' onClick={handleClickOffModal}>
       <div id='gallery-modal-content'>
-        <FaCompress id='gallery-modal-close' onClick={handleCollapseImageClick} />
-        {(imgIndex > 0) ? <FaChevronLeft className='gallery-modal-arrow' onClick={handleCarouselLeftClick} /> : <FaChevronLeft className='hidden gallery-modal-arrow' />}
-        <div style={{ height: '100%', width: '40%', overflow: 'hidden'}}>
-          <InnerImageZoom src={selectedStyle.photos[imgIndex].url} zoomScale={2} />
+        <div>
+          <FaCompress id='gallery-modal-close' onClick={handleCollapseImageClick} />
+          {(imgIndex > 0) ? <FaChevronLeft className='gallery-modal-arrow' onClick={handleCarouselLeftClick} /> : <FaChevronLeft className='hidden gallery-modal-arrow' />}
+          <div style={{ height: '100%', width: '40%', overflow: 'hidden'}}>
+            <InnerImageZoom src={selectedStyle.photos[imgIndex].url} zoomScale={2} />
+          </div>
+          {(imgIndex < selectedStyle.photos.length - 1) ? <FaChevronRight className='gallery-modal-arrow' onClick={handleCarouselRightClick} /> : <FaChevronRight className='hidden gallery-modal-arrow' />}
         </div>
-        {(imgIndex < selectedStyle.photos.length - 1) ? <FaChevronRight className='gallery-modal-arrow' onClick={handleCarouselRightClick} /> : <FaChevronRight className='hidden gallery-modal-arrow' />}
+        <SubImageCarousel selectedStyle={selectedStyle} imageClickHandler={imageClickHandler} />
       </div>
     </div>
   )

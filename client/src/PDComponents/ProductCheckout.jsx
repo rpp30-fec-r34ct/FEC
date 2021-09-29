12e1cea5f0cda0ec0fd7b717e0479cb272f7065e
alexanderolvera <alexander.olvera943@gmail.com>
2021-09-28 21:20:31 -0500

    Plemented functionality for favorite button

--- client/src/PDComponents/ProductCheckout.jsx
+++ client/src/PDComponents/ProductCheckout.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useParams } from 'react-router'
 import SizeSelector from './SizeSelector.jsx'
 import QuanitiySelector from './QuanitySelector.jsx'
 import AddToCart from './AddToCart.jsx'
@@ -6,6 +7,7 @@ import AddToCart from './AddToCart.jsx'
 const ProductCheckoutComponent = ({ selectedStyle }) => {
   const [selectedSize, setSelectedSize] = useState(null)
   const [selectedSku, setSelectedSku] = useState(null)
+  const { productId } = useParams()
 
   const handleSizeChange = (e) => {
     e.preventDefault()
@@ -15,13 +17,22 @@ const ProductCheckoutComponent = ({ selectedStyle }) => {
 
   const handleNoSkuClick = (e) => {
     e.preventDefault()
-    // focus on drop down
     document.querySelector('#style-select').focus()
   }
 
   const updateSelectedSize = (newSelection, sizeId) => {
     setSelectedSize(newSelection)
     setSelectedSku(sizeId)
+  }
+
+  const handleFavoriteButtonClick = () => {
+    console.log('click!')
+    let outfitStorage = JSON.parse(localStorage.getItem('FECOutfit'));
+    if (outfitStorage === null) {
+      outfitStorage = []
+    }
+    outfitStorage.push(productId)
+    localStorage.setItem('FECOutfit', JSON.stringify(outfitStorage))
   }
 
   const checkoutStyles = {
@@ -31,7 +42,7 @@ const ProductCheckoutComponent = ({ selectedStyle }) => {
 
   const containerStyles = { display: 'flex', justifyContent: 'space-evenly', marginTop: '2em' }
 
-  const buttonStyles = {height: '2em', width: '10em', borderRaidus: 0, textAlign: 'center', backgroundColor: 'transparent', border: 'solid 1px black'}
+  const buttonStyles = {height: '2em', width: '10em', borderRaidus: 0, textAlign: 'center', backgroundColor: 'transparent', border: 'solid 1px black', cursor: 'pointer'}
   return (
     <div style={checkoutStyles}>
       <div style={containerStyles}>
@@ -42,7 +53,7 @@ const ProductCheckoutComponent = ({ selectedStyle }) => {
 
       <div style={containerStyles}>
         <AddToCart sku={selectedSku} handleNoSkuClick={handleNoSkuClick} />
-        <button style={buttonStyles}>Favorite</button>
+        <button style={buttonStyles} onClick={handleFavoriteButtonClick}>Favorite</button>
       </div>
 
     </div>

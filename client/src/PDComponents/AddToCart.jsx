43a85ae5a5c3d7b9ee6f15b77f497f9dbcfc1ad2
Alexander Olvera <alexander.olvera943@gmail.com>
2021-09-30 17:00:19 -0500

    Updated styles for checkout buttons

--- client/src/PDComponents/AddToCart.jsx
+++ client/src/PDComponents/AddToCart.jsx
@@ -2,26 +2,24 @@ import axios from 'axios'
 import React from 'react'
 
 const AddToCartComponent = ({ sku, selectedSize, handleNoSkuClick }) => {
-
   const handleAddToCartClick = (e) => {
     e.preventDefault()
     console.log('click')
     axios.post('/api/cart', {
       sku_id: sku
     })
-    .then(response => {
-      console.log('cart api ', response.data)
-    })
-    .catch(err => console.log(err))
+      .then(response => {
+        console.log('cart api ', response.data)
+      })
+      .catch(err => console.log(err))
   }
 
-  return(
+  return (
     <>
-    {sku
-    ? <>{selectedSize.quantity > 0 && <button onClick={handleAddToCartClick}> Add to cart +</button>}</>
+      {sku
+        ? <>{selectedSize.quantity > 0 && <button className='checkout-button' onClick={handleAddToCartClick}>Cart +</button>}</>
 
-    : <button onClick={handleNoSkuClick}> Add to cart +</button>
-    }
+        : <button className='checkout-button' onClick={handleNoSkuClick}>Cart +</button>}
     </>
 
   )

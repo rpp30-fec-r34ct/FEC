2efa4ac417f7c4de4701062fb24c9716fa32253a
Ashley Reischman <ashleyreischman648@gmail.com>
2021-10-04 10:07:04 -0600

    added everything but the photo upload section to the reviw modal and fixed css so the content of the form is aligned

--- client/src/components/Review Widget/AddReviewModal.jsx
+++ client/src/components/Review Widget/AddReviewModal.jsx
@@ -37,11 +37,31 @@ const AddReviewModal = (props) => {
 
   const reviewFormStyle = {
     width: '95%',
-    margin: 'auto'
+    margin: '50px auto',
+
   }
 
   const closeOutIconStyle = {
-    color: 'black'
+    color: 'black',
+    marginLeft: '97%'
+  }
+
+  const subTextStyle = {
+    color: 'grey',
+    opacity: '0.8',
+    fontSize: '12px'
+  }
+
+  const addReviewTitleStyle = {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    margin: '10px',
+    padding: '5px'
+  }
+
+  const addReviewItemStyle = {
+    marginLeft: '20px',
+    marginBottom: '10px'
   }
 
   const getProductName = async (id) => {
@@ -81,54 +101,60 @@ const AddReviewModal = (props) => {
       <>
         <div style={pageCoverStyle} />
         <div data-testid='testModalContainer' style={modalContainerStyle}>
-        <span>{'Write Your Review'}</span><br/>
-        <span>{'About the ' + productName}</span>
+        <i className='fas fa-times' onClick={props.onAddReviewClick} style={closeOutIconStyle} />
+          <span style={{fontSize:'25px', fontWeight: 'bold', margin: 'auto'}}>{'Write Your Review'}</span>
+          <br></br>
+          <span style={{fontSize:'20px', margin: '-25px auto'}}>{'About the ' + productName}</span>
           <form style={reviewFormStyle}>
-            <label> Overall Rating*
+            <label style={addReviewTitleStyle}>1. Overall Rating: *</label>
               <AddReviewStars />
-            </label>
             <div>
-              <p>Do you recommend this product?</p>
-              <input type="radio" id="yesRecommend" name="yesRecommend" value="Yes"/>
-              <label htmlFor="yesRecommend">Yes</label><br></br>
-              <input type="radio" id="noRecommend" name="noRecommend" value="No"/>
-              <label htmlFor="noRecommend"> No</label><br></br>
-            </div>
-            <div>
-              <label> Characteristics*
-                <div>{getCharacteristics(props.reviewsMeta)}</div>
-              </label>
-            </div>
-            <div>
-              <label> Review Summary
+              <span style={addReviewTitleStyle}>2. Do you recommend this product?: *</span>
               <br></br>
-                <input type="textarea" maxLength="60" placeholder="Example: Best purhcase ever!" style={{width: '90%', height: '30px'}}/>
-              </label>
+              <div style={addReviewItemStyle}>
+                <input type="radio" id="yesRecommend" name="yesRecommend" value="Yes"/>
+                <label htmlFor="yesRecommend">Yes</label><br></br>
+                <input type="radio" id="noRecommend" name="noRecommend" value="No"/>
+                <label htmlFor="noRecommend"> No</label>
+                <br></br>
+              </div>
             </div>
             <div>
-              <label> Review Body
-                <br></br>
-                <textarea onChange={onReviewBodyChange} maxLength="1000" minLength="50" placeholder="Why did you like the product or not?" style={{width: '90%', height: '100px'}}></textarea>
-                <br></br>
-                <span style={{color: 'grey', opacity: '0.6'}}>{getCharCountDisplay()}</span>
-              </label>
+              <label style={addReviewTitleStyle}>3. Characteristics: *</label>
+              <div style={{width: '50%', margin: 'auto'}}>{getCharacteristics(props.reviewsMeta)}</div>
             </div>
-
-            {/* <label> Review Body *
-              <input type="text" />
-            </label>
-            <label> Upload your photos
-              <input type="text" />
-            </label>
-            <label> What is your nickname? *
-              <input type="text" />
-            </label>
-            <label> Your email *
-              <input type="text" />
-            </label>
-            <input type="submit" value="Submit" /> */}
+            <div>
+              <label style={addReviewTitleStyle}>4. Review Summary:</label><br></br>
+              <div style={addReviewItemStyle}>
+                <input type="textarea" maxLength="60" placeholder="Example: Best purhcase ever!" style={{width: '90%', height: '30px', fontFamily: 'Playfair Display serif'}}/>
+              </div>
+            </div>
+            <div>
+              <label style={addReviewTitleStyle}>5. Review Body: *</label><br></br>
+              <div style={addReviewItemStyle}>
+                <textarea onChange={onReviewBodyChange} maxLength="1000" minLength="50" placeholder="Why did you like the product or not?" style={{width: '90%', height: '100px', fontFamily: 'Playfair Display serif'}}></textarea>
+                <br></br>
+                <span style={subTextStyle}>{getCharCountDisplay()}</span>
+              </div>
+            </div>
+            <div>
+              <label style={addReviewTitleStyle}>6. What is your nickname?: *</label><br></br>
+              <div style={addReviewItemStyle}>
+                <input type="text" maxLength="60" placeholder="Example: jackson11!" style={{width: '70%', fontFamily: 'Playfair Display serif'}}/>
+                <br></br>
+                <span style={subTextStyle}>{'For privacy reasons, do not use your full name or email address'}</span>
+              </div>
+            </div>
+            <div>
+              <label style={addReviewTitleStyle}>7. Your email: *</label><br></br>
+              <div style={addReviewItemStyle}>
+                <input type="text" maxLength="60" style={{width: '70%', fontFamily: 'Playfair Display serif'}} placeholder="Example: jackson11@email.com"/>
+                <br></br>
+                <span style={subTextStyle}>{'For authentication reasons, you will not be not be emailed'}</span>
+              </div>
+            </div>
+            <input className="removeFiltersBtn" type="submit" value="Submit" />
           </form>
-          <i className='fas fa-times' onClick={props.onAddReviewClick} style={closeOutIconStyle} />
         </div>
       </>
     )

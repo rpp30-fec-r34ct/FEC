d7fd4327a1cca0c9775a13ab28f9d1f5f34842af
Ashley Reischman <ashleyreischman648@gmail.com>
2021-10-08 05:55:52 -0700

    added additional tests for prodcut breakdown section

--- tests/components/RatingsAndReviewsTests/productBreakDown.test.js
+++ tests/components/RatingsAndReviewsTests/productBreakDown.test.js
@@ -27,4 +27,20 @@ describe('Product Breakdown Component', () => {
     const productBreakDownBar = screen.getByTestId('testProductBreakdownBar')
     expect(productBreakDownBar).toHaveClass('productBreakDownBar')
   })
+
+  test('Should have the correct middle limit for the given characteristic', function () {
+    render(<ProductBreakdown characteristicsData={data} />)
+    expect(screen.getByText('Perfect')).toBeInTheDocument()
+  })
+
+  test('Should have the correct lower limit for the given characteristic', function () {
+    render(<ProductBreakdown characteristicsData={data} />)
+    expect(screen.getByText('Runs tight')).toBeInTheDocument()
+  })
+
+  test('Should have the correct upper limit for the given characteristic', function () {
+    render(<ProductBreakdown characteristicsData={data} />)
+    expect(screen.getByText('Runs long')).toBeInTheDocument()
+  })
+
 })

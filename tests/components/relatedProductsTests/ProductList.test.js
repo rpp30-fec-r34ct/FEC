9a972cb1328e652267f06040c198cf1fc81cd507
Laweeza <louieshing@gmail.com>
2021-10-07 17:30:18 -0700

    finished productList test

--- tests/components/relatedProductsTests/ProductList.test.js
+++ tests/components/relatedProductsTests/ProductList.test.js
@@ -5,12 +5,44 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { createMemoryHistory } from 'history'
+import { Router, Route, Switch } from 'react-router-dom'
 import testRelatedProducts from '../../../testData/testRelatedProducts'
+import testProduct from '../../../testData/testProduct'
 import ProductList from '../../../client/src/components/relatedProducts/ProductList.jsx'
 
 describe('Product List Component', () => {
   test('should render list of 4 products ', () => {
-    const ProductListComponent = render(<ProductList product={testRelatedProducts} />)
-    expect(ProductListComponent.container.querySelector('.product-list')).toBeInTheDocument()
+    const history = createMemoryHistory()
+    const route = '/product/47421/carousel'
+    history.push(route)
+    const ProductListComponent = render(
+      <Router history={history}>
+        <Switch>
+          <Route path='/product/:productId/carousel/'>
+            <ProductList currentIndex={1} relatedProducts={testRelatedProducts} currentOverview={testProduct} />
+          </Route>
+        </Switch>
+      </Router>
+    )
+
+    expect(ProductListComponent.container.querySelector('.carousel-content').children.length).toEqual(4)
+  })
+
+  test('should render nothing if no products ', () => {
+    const history = createMemoryHistory()
+    const route = '/product/47421/carousel'
+    history.push(route)
+    const ProductListComponent = render(
+      <Router history={history}>
+        <Switch>
+          <Route path='/product/:productId/carousel/'>
+            <ProductList currentIndex={0} relatedProducts={[]} currentOverview={testProduct} />
+          </Route>
+        </Switch>
+      </Router>
+    )
+
+    expect(ProductListComponent.container.querySelector('.carousel-content').children.length).toEqual(0)
   })
 })

ab6c48973e24d6b016bc6811f2f0f4c9268f49ef
Ashley Reischman <ashleyreischman648@gmail.com>
2021-10-08 17:58:33 -0700

    added tests and id for review recommend component

--- client/src/components/Review Widget/AddReview/AddReviewsRecommended.jsx
+++ client/src/components/Review Widget/AddReview/AddReviewsRecommended.jsx
@@ -3,10 +3,10 @@ import '../cssFiles/addReview.css'
 
 const AddReviewsRecommended = (props) => {
   return (
-    <div className='addReviewItem'>
-      <input onChange={props.handleReviewRecommendChange} type='radio' id='yesRecommend' name='yesRecommend' value='Yes' defaultChecked />
+    <div data-testid="testAddReviewItem" className='addReviewItem'>
+      <input data-testid="testyesRecommend" onChange={props.handleReviewRecommendChange} type='radio' id='yesRecommend' name='yesRecommend' value='Yes' defaultChecked />
       <label htmlFor='yesRecommend'>Yes</label><br />
-      <input onChange={props.handleReviewRecommendChange} type='radio' id='noRecommend' name='yesRecommend' value='No' />
+      <input data-testid="testnoRecommend" onChange={props.handleReviewRecommendChange} type='radio' id='noRecommend' name='yesRecommend' value='No' />
       <label htmlFor='noRecommend'> No</label>
       <br />
     </div>

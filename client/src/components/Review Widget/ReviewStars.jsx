409a4a3984cc1a142cdf324e07c00906e90f3a46
Ashley Reischman <ashleyreischman648@gmail.com>
2021-10-09 00:25:14 -0700

    added alt tags to all images in reviews ection for accessbility

--- client/src/components/Review Widget/ReviewStars.jsx
+++ client/src/components/Review Widget/ReviewStars.jsx
@@ -14,21 +14,21 @@ const ReviewStars = (props) => {
   const decimalRating = props.starRating % 1
 
   for (let i = 0; i < wholeStars.length; i++) {
-    wholeStars[i] = <img key={props.review_id + i} src={fullStar} />
+    wholeStars[i] = <img alt="whole-review-stars" key={props.review_id + i} src={fullStar} />
   }
 
   if (decimalRating > 0 && decimalRating <= 0.3) {
-    decimalStars.push(<img key={props.review_id + decimalStars.length - 1} src={quarterStar} />)
+    decimalStars.push(<img alt="decimal-review-stars" key={props.review_id + decimalStars.length - 1} src={quarterStar} />)
   } else if (decimalRating > 0.3 && decimalRating <= 0.6) {
-    decimalStars.push(<img key={props.review_id + decimalStars.length - 1} src={halfStar} />)
+    decimalStars.push(<img alt="decimal-review-stars" key={props.review_id + decimalStars.length - 1} src={halfStar} />)
   } else if (decimalRating > 0.6 && decimalRating <= 0.9) {
-    decimalStars.push(<img key={props.review_id + decimalStars.length - 1} src={threeQuarterStar} />)
+    decimalStars.push(<img alt="decimal-review-stars" key={props.review_id + decimalStars.length - 1} src={threeQuarterStar} />)
   }
 
   starArray = wholeStars.concat(decimalStars)
   if (starArray.length < 5) {
     for (let i = starArray.length; i < 5; i++) {
-      starArray.push(<img key={props.review_id + i} src={emptyStar} />)
+      starArray.push(<img alt="whole-review-stars" key={props.review_id + i} src={emptyStar} />)
     }
   }
 
